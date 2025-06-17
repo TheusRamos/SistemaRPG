44396cb7f0e405493e095f62572ac4b909cce8b6
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-06-16 21:56:52 -0300

    Update Mago.java

--- src/main/java/Heros/Mago.java
+++ src/main/java/Heros/Mago.java
@@ -14,6 +14,15 @@ import java.util.List;
 public class Mago extends Personagem {
     private static final long serialVersionUID = 1L;
     
+
+    @Override
+    public boolean estaVivo() {
+        if(this.vidaAtual > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
     public Mago(String nome ){
         super(nome, "Mestres do arcano, os Magos manipulam as forças da realidade como um artista manipula suas tintas. "
                 + "Para eles, o campo de batalha é uma tela em branco onde pintam com fogo, gelo e relâmpagos. "
@@ -22,12 +31,12 @@ public class Mago extends Personagem {
                 + "seja frágil," + "sua mente é uma arma de destruição em massa, fazendo deles os mais temidos causadores de "
                 + "dano à distância. Escolher o caminho do Mago é escolher o poder em sua forma mais pura e volátil.", 550, 10, 0, 0, 2, 10, 2, 0);
     }
-    // Dentro da classe Mago.java
+    
 
 @Override
 public void atacar(Personagem alvo) {
     System.out.println(this.nome + " lança uma bola de fogo em " + alvo.getNome() + "!");
-    // Lógica de dano simples baseada na inteligência
+    
     int dano = this.inteligencia; 
     alvo.receberDano(dano);
 }
